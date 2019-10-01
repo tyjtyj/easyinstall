3a56e124da62be4e67a68f37e14dd1d1056b67b4
Agneev Mukherjee <agneev@icloud.com>
2019-10-01 17:22:53 +0530

    Output is now hidden

--- trakt.sh
+++ trakt.sh
@@ -3,15 +3,15 @@ clear
 echo "Now downloading Plex Trakt Scrobbler"
 echo "Changing working directory to /tmp"
 cd /tmp
-wget https://github.com/trakt/Plex-Trakt-Scrobbler/archive/master.zip -O Plex-Trakt-Scrobbler.zip
+wget https://github.com/trakt/Plex-Trakt-Scrobbler/archive/master.zip -O Plex-Trakt-Scrobbler.zip > /dev/null 2>&1
 echo "Trakt downloaded"
-unzip Plex-Trakt-Scrobbler.zip
+unzip Plex-Trakt-Scrobbler.zip > /dev/null 2>&1
 sudo cp -r Plex-Trakt-Scrobbler-*/Trakttv.bundle "/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-ins/"
 echo "Trakt installed"
 sudo rm -r Plex-Trakt-Scrobbler.zip
 sudo rm -r Plex-Trakt-Scrobbler-*
-wget https://www.dropbox.com/s/jo9jam8n73htkqc/trakt.zip?dl=1 -O trakt.zip
-unzip trakt.zip
+wget https://www.dropbox.com/s/jo9jam8n73htkqc/trakt.zip?dl=1 -O trakt.zip > /dev/null 2>&1
+unzip trakt.zip > /dev/null 2>&1
 cd trakt
 sudo mv apsw.so /usr/lib/plexmediaserver/Resources/Python/lib/python2.7/lib-dynload
 echo "Moved apsw.so"
@@ -22,7 +22,7 @@ sudo mv com.plexapp.plugins.trakttv.db "/var/lib/plexmediaserver/Library/Applica
 sudo mv com.plexapp.plugins.trakttv.db-shm "/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-in Support/Databases"
 sudo mv com.plexapp.plugins.trakttv.db-wal "/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-in Support/Databases"
 echo "Moved databases"
-sudo systemctl restart plexmediaserver
+sudo systemctl restart plexmediaserver > /dev/null 2>&1
 echo "Restarted Plex"
 echo "Completed"
 echo "Now manually delete the 'trakt' folder"
