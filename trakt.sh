2dd56c0128841212407983a92f892b0339fa699c
Agneev Mukherjee <agneev@icloud.com>
2019-10-01 17:26:58 +0530

    Update trakt.sh

--- trakt.sh
+++ trakt.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 clear
 echo "Now downloading Plex Trakt Scrobbler"
+echo "."
 echo "Changing working directory to /tmp"
 cd /tmp
 wget https://github.com/trakt/Plex-Trakt-Scrobbler/archive/master.zip -O Plex-Trakt-Scrobbler.zip > /dev/null 2>&1
 echo "Trakt downloaded"
 unzip Plex-Trakt-Scrobbler.zip > /dev/null 2>&1
 sudo cp -r Plex-Trakt-Scrobbler-*/Trakttv.bundle "/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-ins/"
+echo "."
 echo "Trakt installed"
 sudo rm -r Plex-Trakt-Scrobbler.zip
 sudo rm -r Plex-Trakt-Scrobbler-*
@@ -22,6 +24,7 @@ sudo mv com.plexapp.plugins.trakttv.db "/var/lib/plexmediaserver/Library/Applica
 sudo mv com.plexapp.plugins.trakttv.db-shm "/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-in Support/Databases"
 sudo mv com.plexapp.plugins.trakttv.db-wal "/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-in Support/Databases"
 echo "Moved databases"
+echo "."
 sudo systemctl restart plexmediaserver > /dev/null 2>&1
 echo "Restarted Plex"
 echo "Completed"
