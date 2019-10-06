31497470b0432666a5c3e0498d0694f345e929f8
Agneev Mukherjee <agneev@icloud.com>
2019-10-06 14:37:24 +0530

    -

--- easyinstall.sh
+++ easyinstall.sh
@@ -58,7 +58,7 @@ echo "Install Plex-trakt-scrobbler? (y/N)?"
 read query
 if [ $query = "y" ]
 then
-  curl https://raw.githubusercontent.com/agneevX/easyinstall/master/trakt.sh | sudo bash
+  curl https://raw.githubusercontent.com/agneevX/easyinstall/master/ei_trakt.sh | sudo bash
 else
   printf "Skipping Plex-Trakt-Scrobbler installation"
 fi
@@ -129,36 +129,7 @@ echo "Install qBittorrent? (y/N)"
 read query
 if [ $query = "y" ]
 then
-  clear
-  printf "Now downloading qBittorrent..."
-  sleep 2
-  sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable
-  sudo apt-get update > /dev/null
-  sudo apt-get install qbittorrent-nox -y > /dev/null
-  clear
-  echo "Install qbittorrent.service? (y/N)?"
-  read query
-  if [ $query = "y" ]
-  then
-    cd /tmp
-    wget https://raw.githubusercontent.com/agneevX/easyinstall/master/qbittorrent.service -O qbittorrent.service
-    sed -i "s/qbtuser/$USER/g" /tmp/qbittorrent.service
-    sudo cp /tmp/qbittorrent.service /etc/systemd/system
-    sudo systemctl enable qbittorrent.service
-    sudo systemctl start qbittorrent.service
-    exit 0
-  else
-    exit 1
-  fi
-  if [ $? -eq 0 ]
-  then
-    printf "qBittorrent installed and running at port 8080"
-    printf "username: admin, password: adminadmin"
-  elif [ $? -eq 1 ]
-  then
-    printf "qBittorrent installed"
-    printf "username: admin, password: adminadmin"
-   fi
+  curl https://raw.githubusercontent.com/agneevX/easyinstall/master/ei_qbittorrent.sh | sudo bash
 else
   printf "Not installing qBittorrent"
 fi
