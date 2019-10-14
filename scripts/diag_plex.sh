3705d5400a88c80ec3e5d0da7eab8c1694510bcf
agneev <agneev@icloud.com>
2019-10-14 15:11:39 +0530

    Corrected errors

--- scripts/diag_plex.sh
+++ scripts/diag_plex.sh
@@ -2,7 +2,7 @@
 
 IP_ADDRESS=$(ifconfig eth0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*')
 clear
-Now "Now downloading Plex"
+echo "Now downloading Plex"
 sleep 3
 echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
 curl https://downloads.plex.tv/plex-keys/PlexSign.key | sudo apt-key add -
@@ -14,8 +14,8 @@ sed -i 's/#AllowTCPForwarding yes/AllowTCPForwarding yes/g' /etc/ssh/sshd_config
 sed -i 's/#PermitOpen any/PermitOpen any/g' /etc/ssh/sshd_config
 sudo service ssh restart
 clear
-printf "Open a terminal and copy-paste this:\n"
-echo "ssh -L 32400:localhost:32400 $USER@$IP_ADDRESS"
+printf "Open a terminal and copy-paste this:\n\n"
+printf "ssh -L 32400:localhost:32400 $USER@$IP_ADDRESS\n"
 printf "Then open up a new browser window and paste this:\n"
 printf "http://$IP_ADDRESS:32400/web\n\n"
 printf "After that is done, append '#' to:\nAllowTCPForwarding yes\nPermitOpen any\n"
